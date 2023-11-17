bcf8502d8df8732ef84c4c29a3a624d1f8237924
sissamrcorreia <115619893+sissamrcorreia@users.noreply.github.com>
2023-11-17 11:29:35 +0000

    ler

--- proj1.cpp
+++ proj1.cpp
@@ -7,8 +7,31 @@
 
 #include <iostream>
 #include <vector>
+//#include <stdio.h>
+//#include <string.h>
+
+// void print valor máximo que pode ser obtido a partir da chapa dada como input
+
+// void cleanupMemory() { delete[] pieces;}
+
 
 int main() {
+    // ler dimensoes da placa
+    int X, Y;
+    std::cin >> X >> Y;
 
+    // ler numero de tipos de pecas
+    int N;
+    std::cin >> N;
+
+    // criar um vetor para armazenar as pecas
+    std::vector<int> pieces(N);
+
+    for (int i = 0; i < N; ++i) {
+        int ai, bi, pi;
+        std::cin >> ai >> bi >> pi;
+
+    }
+    
     return 0;
 }
